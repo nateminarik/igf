43a39a02fd4ac2becdaa763e400ed805a33b2354
Evgen I <evgenivanov90@gmail.com>
2014-07-24 17:06:20 +0400

    [+] services page

--- root/js/script.js
+++ root/js/script.js
@@ -11,17 +11,42 @@ $(function () {
 $(function () {
     var mapSpringfield = new GMaps({
             el: '#mapSpringfield',
-            lat: -12.043333,
-            lng: -77.028333
+            lat: 38.743830,
+            lng: -77.202341,
+            zoom: 17,
+            disableDefaultUI: true
         }),
         mapCharleston = new GMaps({
             el: '#mapCharleston',
-            lat: -12.043333,
-            lng: -77.028333
+            lat: 32.796129,
+            lng: -79.996165
         }),
         mapAtlanta = new GMaps({
             el: '#mapAtlanta',
-            lat: -12.043333,
-            lng: -77.028333
+            zoom: 13,
+            lat: 33.734312,
+            lng: -84.566143
         });
+
+    mapSpringfield.addMarker({
+        lat: 38.743830,
+        lng: -77.202341,
+        infoWindow: {
+            content: '7520 Fullerton Rd <br>Springfield,VA 22153, <br><strong>+1 703-569-4520</strong>'
+        }
+    });
+    mapCharleston.addMarker({
+        lat: 32.796129,
+        lng: -79.996165,
+        infoWindow: {
+            content: '1105 Magnolia Rd <br>Charleston, SC 29407, <br><strong>+1 843-573-2220</strong>'
+        }
+    });
+    mapAtlanta.addMarker({
+        lat: 33.734291,
+        lng: -84.566141,
+        infoWindow: {
+            content: '5550 Fulton Industrial Blvd SW,<br>Atlanta, GA 30336, <br><strong>+1 404-349-6333</strong>'
+        }
+    });
 });
