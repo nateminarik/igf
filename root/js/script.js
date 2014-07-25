80ba3f73bb0cd84a4bf7d3e354d8ea57c4b24c23
Evgen I <evgenivanov90@gmail.com>
2014-07-25 18:31:15 +0400

    [+] events page

--- root/js/script.js
+++ root/js/script.js
@@ -9,51 +9,83 @@ $(function () {
 });
 
 $(function () {
+    // ----- services page maps -----------
     if ($('#services').length) {
-    var mapSpringfield = new GMaps({
-            el: '#mapSpringfield',
+        var mapSpringfield = new GMaps({
+                el: '#mapSpringfield',
+                lat: 38.743830,
+                lng: -77.202341,
+                zoom: 17,
+                disableDefaultUI: true
+            }),
+            mapCharleston = new GMaps({
+                el: '#mapCharleston',
+                lat: 32.796129,
+                lng: -79.996165
+            }),
+            mapAtlanta = new GMaps({
+                el: '#mapAtlanta',
+                zoom: 13,
+                lat: 33.734312,
+                lng: -84.566143
+            });
+
+        mapSpringfield.addMarker({
             lat: 38.743830,
             lng: -77.202341,
-            zoom: 17,
-            disableDefaultUI: true
-        }),
-        mapCharleston = new GMaps({
-            el: '#mapCharleston',
-            lat: 32.796129,
-            lng: -79.996165
-        }),
-        mapAtlanta = new GMaps({
-            el: '#mapAtlanta',
-            zoom: 13,
-            lat: 33.734312,
-            lng: -84.566143
+            infoWindow: {
+                content: '7520 Fullerton Rd <br>Springfield,VA 22153, <br><strong>+1 703-569-4520</strong>'
+            }
         });
-
-    mapSpringfield.addMarker({
-        lat: 38.743830,
-        lng: -77.202341,
-        infoWindow: {
-            content: '7520 Fullerton Rd <br>Springfield,VA 22153, <br><strong>+1 703-569-4520</strong>'
-        }
-    });
-    mapCharleston.addMarker({
-        lat: 32.796129,
-        lng: -79.996165,
-        infoWindow: {
-            content: '1105 Magnolia Rd <br>Charleston, SC 29407, <br><strong>+1 843-573-2220</strong>'
-        }
-    });
-    mapAtlanta.addMarker({
-        lat: 33.734291,
-        lng: -84.566141,
-        infoWindow: {
-            content: '5550 Fulton Industrial Blvd SW,<br>Atlanta, GA 30336, <br><strong>+1 404-349-6333</strong>'
-        }
-    });
+        mapCharleston.addMarker({
+            lat: 32.796129,
+            lng: -79.996165,
+            infoWindow: {
+                content: '1105 Magnolia Rd <br>Charleston, SC 29407, <br><strong>+1 843-573-2220</strong>'
+            }
+        });
+        mapAtlanta.addMarker({
+            lat: 33.734291,
+            lng: -84.566141,
+            infoWindow: {
+                content: '5550 Fulton Industrial Blvd SW,<br>Atlanta, GA 30336, <br><strong>+1 404-349-6333</strong>'
+            }
+        });
     }
 });
 
 $(function () {
-    console.log('15');
-    $("a[rel^='prettyPhoto']").prettyPhoto();
+    // ----- gallery page  -----------
+    var gallery = $('#gallery');
+    gallery.photobox('li a',{
+            time: 0
+        });
+    if (gallery) {
+        $(window).load(function() {
+            console.log('fgf');
+            gallery.masonry({
+                    itemSelector: 'li'
+                });
+
+            $(window).on('resize', function () {
+                buildScrollbar();
+            });
+
+            setTimeout(buildScrollbar, 100);
+        });
+        // ----- add scrollbar (to prevent sidebar's height be more than events height)  -----------
+        function buildScrollbar () {
+            var eventsHeight = $('#output').height();
+            var galleryWrp = $('.gallery-wrp');
+
+            if ($(window).outerWidth() >= 753) {
+                $(galleryWrp)
+                    .css('height', eventsHeight)
+                    .perfectScrollbar();
+            } else {
+                $(galleryWrp).css('height', 'auto');
+            }
+        }
+    }
+
 });
